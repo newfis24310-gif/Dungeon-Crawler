6f731b46192c48d3ceb9b83e4f0c4e7470e72834
Newfis <newfis24310@gmail.com>
2026-03-21 16:41:58 +0200

    Enemy progress

--- Assets/Scripts/Classes/Tiles/CombatTile.cs
+++ Assets/Scripts/Classes/Tiles/CombatTile.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class CombatTile : ActionTile
 {
+    CombatComponent combatComponent;
 
+    public override void ExecuteEffect(Player player)
+    {
+        if(traversed == false)
+        {
+            Debug.Log("Oh no! You've entered combat.");
+            combatComponent = player.GetComponent<CombatComponent>();
+            combatComponent.PrepareCombat();
+            traversed = true;
+        }
+    }
 }
